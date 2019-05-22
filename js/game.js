7bf907f1e95d103a883bbc4a4fee81953f3fdf84
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-05-22 18:02:22 +0100

    Add HUD

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -51,6 +53,7 @@ getNewQuestion = () => {
     return window.location.assign("/end.html");
   }
   questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
   const questionIndex = Math.floor(Math.random() * availableQuestions.length); 
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -73,16 +76,22 @@ choices.forEach(choice => {
     const selectedAnswer = selectedChoice.dataset['number'];
     const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
+    if(classToApply === 'correct') {
+      increamentScore(CORRECT_BONUS);
+    };
+
     selectedChoice.parentElement.classList.add(classToApply);
 
     setTimeout(() => {
       selectedChoice.parentElement.classList.remove(classToApply);
       getNewQuestion();
     }, 1000);
-    
-    
   });
 });
 
+increamentScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
 
 startGame();
